9c231020efe1c4836d94398dd3039cb124c1d820
Jay Chetty <jaypaulchetty@gmail.com>
2013-07-03 09:27:04 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraftApp::Application.config.secret_token = '70bffb64dc7512014d58c1a4a0565cf6e50a66ccd25754f5420099fac9273db82bb253e700b3baf4df17f2b0db27b0db9b8f4bb50c30f85a917cfadb6b5925e0'
